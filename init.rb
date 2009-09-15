baf1d4720d05f7eda11596260dee3fdc94cfc3c0
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-15 09:33:27 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_external_signup do
+  name 'Redmine External Signup plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
