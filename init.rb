6749a910195a83f1ae0cdfacabaab6a82a88eb30
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-15 09:45:18 -0700

    [#3016] Configured the init.rb information

--- init.rb
+++ init.rb
@@ -1,8 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_external_signup do
-  name 'Redmine External Signup plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'External Signup'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/external-signup'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'External signup is a Redmine plugin that allows an external source create a new Redmine project and user.'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
 end
