fd81d7811815326ff77203818c77fb352b075d30
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-15 11:58:54 -0700

    [#3014] Created pending tests for all of the different API method calls.

--- test/functional/external_signups_controller_test.rb
+++ test/functional/external_signups_controller_test.rb
@@ -6,4 +6,59 @@ class ExternalSignupsControllerTest < ActionController::TestCase
     should_route :put, "/external_signups", :controller => 'external_signups', :action => 'update'
   end
 
+  context "#create" do
+    context "GET" do
+      should "return a 405 error"
+      should "return an XML document saying to use POST"
+    end
+
+    context "POST" do
+      context "with a valid security token" do
+        context "with valid data" do
+          should "do things"
+        end
+
+        context "with missing data" do
+          should "return a 412 error"
+          should "return an XML document saying what data is missing"
+        end
+      end
+
+      context "with an invalid security token" do
+        should "return a 403 error"
+        should "return an XML document saying the security token is invalid"
+      end
+    end
+
+    context "PUT" do
+      should "return a 405 error"
+      should "return an XML document saying to use POST"
+    end
+
+    context "DELETE" do
+      should "return a 405 error"
+      should "return an XML document saying to use POST"
+    end
+  end
+
+  context "#update" do
+    context "GET" do
+      should "return a 405 error"
+      should "return an XML document saying to use PUT"
+    end
+
+    context "POST" do
+      should "return a 405 error"
+      should "return an XML document saying to use PUT"
+    end
+
+    context "PUT" do
+      should "do things"
+    end
+
+    context "DELETE" do
+      should "return a 405 error"
+      should "return an XML document saying to use PUT"
+    end
+  end
 end
