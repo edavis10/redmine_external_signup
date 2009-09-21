20829d9e27eea8186a21265d3a1645a03f121cc7
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-21 14:06:10 -0700

    [#3016] Added an integration test for updating existing data.

---
+++ test/integration/external_signups_update_integration_test.rb
@@ -0,0 +1,53 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ExternalSignupsUpdateIntegrationTest < ActionController::IntegrationTest
+  def send_request
+    put "/external_signups/", {
+      :security_key => @security_key,
+      :project => {
+        :id => @project.id,
+        :name => "Updated integration project"
+      },
+      :user => {
+        :id => @user.id,
+        :firstname => 'Updated user',
+        :lastname => 'Test',
+        :mail => 'test@example.com'
+      }
+    }
+  end
+
+  context "posting from a remote form" do
+    setup do
+      # Need to call #anonymous so the anonymous user is
+      # created before the assert_difference
+      User.anonymous
+      setup_plugin_configuration
+      @project = Project.generate!
+      @user = User.generate_with_protected!
+      @member = Member.generate!({
+                                   :role_ids => @configured_roles.collect(&:id),
+                                   :user => @user,
+                                   :project => @project
+                                 })
+
+    end
+
+    should 'update a project' do
+      send_request
+      @project.reload
+      assert_equal "Updated integration project", @project.name
+    end
+
+    should 'update a user' do
+      send_request
+      @user.reload
+      assert_equal "Updated user", @user.firstname
+    end
+
+    should 'return an xml message to the caller' do
+      send_request
+      assert_equal 'application/xml', @response.content_type
+    end
+  end
+end
